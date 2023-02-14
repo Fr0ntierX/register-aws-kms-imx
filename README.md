48af0671133e04d826eb275c1b39b056c4367330
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2023-02-14 22:07:00 +0100

    IMX registration script

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# register-aws-kms-imx
-A script to register AWS KMS wallets on ImmutableX
+# Register a wallet controlled by an AWS KMS key on ImmutableX
+
+This script registers a wallet controlled by an AWS KMS key on ImmutableX.
+
+## AWS KMS key setup
+
+You need to create an asymmetric AWS KMS key compatible with the Ethereum network. Use the following settings:
+
+<!-- Image showing the settings -->
+
+![AWS KMS key settings](images/aws_kmw_setup.jpg)
+
+## Setup AWS profile
+
+The recommended way to use the script is to setup an AWS profile in the console running the script. Alternatively, you can set the `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` environment variables.
+
+## Configure environment variables
+
+Create a copy of the `.env.example` file and name it `.env`. Then, fill in the following environment variables:
+
+-   `AWS_KMS_REGION`: the region of the AWS KMS key
+-   `AWS_KMS_KEY_ID`: the ID of the AWS KMS key
+
+The other environment variables are optional.
+
+## Preparing the script
+
+Install the dependencies of the script:
+
+```bash
+yarn install
+```
+
+## Run the script
+
+To run the script execute the following command:
+
+```bash
+yarn register
+```
---
+++ scripts/register.ts
@@ -0,0 +1,45 @@
+import { KMS } from "aws-sdk";
+import { Config, createStarkSigner, generateLegacyStarkPrivateKey, ImmutableX } from "@imtbl/core-sdk";
+import { KMSSigner } from "@rumblefishdev/eth-signer-kms";
+import { ethers } from "ethers";
+
+const main = async () => {
+    // Initialize the AWS KMS key
+    const accessKeyId = process.env.AWS_KMS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_KMS_SECRET_ACCESS_KEY;
+    const credentials = accessKeyId && secretAccessKey ? { accessKeyId, secretAccessKey } : undefined;
+
+    const kms = new KMS({
+        region: process.env.AWS_KMS_REGION,
+        credentials,
+    });
+
+    // Initialize the signer for L1
+    const provider = new ethers.providers.AlchemyProvider(process.env.NETWORK, process.env.ALCHEMY_API_KEY);
+    const ethSigner = new KMSSigner(provider, String(process.env.AWS_KMS_KEY_ID), kms);
+    console.log("Signer Address:", await ethSigner.getAddress());
+
+    // Initialize the signer for L2
+    const starkPrivateKey = await generateLegacyStarkPrivateKey(ethSigner);
+    const starkSigner = createStarkSigner(starkPrivateKey);
+
+    // Initialize the ImmutableX Core SDK
+    const imx = new ImmutableX(process.env.NETWORK === "mainnet" ? Config.PRODUCTION : Config.SANDBOX);
+
+    // Register the account on IMX
+    try {
+        console.info("Waiting for signer registration...");
+        await imx.registerOffchain({
+            ethSigner,
+            starkSigner,
+        });
+        console.log("Signer registered successfully!");
+    } catch (error: any) {
+        console.log("Couldn't resgister account:", error.code);
+    }
+};
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
